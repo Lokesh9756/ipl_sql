2658268d5b33bcf0b9e2897459d56c04f8e8ae4e
Lokesh Pal <lokeshpal9756@gmail.com>
2021-07-07 14:39:58 +0530

    search file added

--- jsfiles/matches.js
+++ jsfiles/matches.js
@@ -1,7 +1,7 @@
 try {
-  const TeamArray = [];
-  const UmpireArray = [];
-  let Data = [];
+  const teamArray = [];
+  const umpireArray = [];
+  let data = [];
   let i;
   let j;
   // To create connection
@@ -14,61 +14,61 @@ try {
       if (CsvObject) {
         // convert csv object into string
         const TempArray = CsvObject.toString().split('\r');
-        Data = TempArray[0].split(',');
+        data = teamArray[0].split(',');
         for (i = 1; i < TempArray.length; i++) {
-          Data.push(TempArray[i].split(','));
+          data.push(TempArray[i].split(','));
         }
         // Array to store  teams
 
-        for (i = 18; i < Data.length - 1; i++) {
-          if (Data[i][10]) {
-            if (TeamArray.includes(Data[i][10]));
+        for (i = 18; i < data.length - 1; i++) {
+          if (data[i][10]) {
+            if (teamArray.includes(data[i][10]));
             else {
-              TeamArray.push(Data[i][10]);
+              teamArray.push(data[i][10]);
             }
           }
         }
         // Array to store Umpire
-        for (i = 18; i < Data.length - 1; i++) {
-          if (Data[i][15]) {
-            if (UmpireArray.includes(Data[i][15]));
+        for (i = 18; i < data.length - 1; i++) {
+          if (data[i][15]) {
+            if (umpireArray.includes(data[i][15]));
             else {
-              UmpireArray.push(Data[i][15]);
+              umpireArray.push(data[i][15]);
             }
           }
         }
-        let Team1;
-        let Team2;
-        let TossWinner;
-        let Winner;
-        let Umpire1;
-        let Umpire2;
+        let team1;
+        let team2;
+        let tossWinner;
+        let winner;
+        let umpire1;
+        let umpire2;
 
-        for (i = 18; i < Data.length - 1; i++) {
-          const season = parseInt(Data[i][1]);
-          const DlApplied = parseInt(Data[i][9]);
-          const WinByRuns = parseInt(Data[i][11]);
-          const WinByWickets = parseInt(Data[i][12]);
-          for (j = 0; j < TeamArray.length - 1; j++) {
-            if (TeamArray[j] === Data[i][4]) {
-              Team1 = j + 1;
+        for (i = 18; i < data.length - 1; i++) {
+          const season = parseInt(data[i][1]);
+          const DlApplied = parseInt(data[i][9]);
+          const WinByRuns = parseInt(data[i][11]);
+          const WinByWickets = parseInt(data[i][12]);
+          for (j = 0; j < teamArray.length - 1; j++) {
+            if (teamArray[j] === data[i][4]) {
+              team1 = j + 1;
             }
-            if (TeamArray[j] === Data[i][5]) {
-              Team2 = j + 1;
+            if (teamArray[j] === data[i][5]) {
+              team2 = j + 1;
             }
-            if (TeamArray[j] === Data[i][6]) {
-              TossWinner = j + 1;
+            if (teamArray[j] === data[i][6]) {
+              tossWinner = j + 1;
             }
-            if (TeamArray[j] === Data[i][10]) {
-              Winner = j + 1;
+            if (teamArray[j] === data[i][10]) {
+              winner = j + 1;
             }
           }
-          for (j = 0; j < UmpireArray.length - 1; j++) {
-            if (UmpireArray[j] === Data[i][15]) {
-              Umpire1 = j + 1;
+          for (j = 0; j < umpireArray.length - 1; j++) {
+            if (umpireArray[j] === data[i][15]) {
+              umpire1 = j + 1;
             }
-            if (UmpireArray[j] === Data[i][16]) {
-              Umpire2 = j + 1;
+            if (umpireArray[j] === data[i][16]) {
+              umpire2 = j + 1;
             }
           }
           // Looped query to insert data into table match table
@@ -77,21 +77,21 @@ try {
             values: [
               i - 17,
               season,
-              Data[i][2],
-              Data[i][3],
-              Team1,
-              Team2,
-              TossWinner,
-              Data[i][7],
-              Data[i][8],
+              data[i][2],
+              data[i][3],
+              team1,
+              team2,
+              tossWinner,
+              data[i][7],
+              data[i][8],
               DlApplied,
-              Winner,
+              winner,
               WinByRuns,
               WinByWickets,
-              Data[i][13],
-              Data[i][14],
-              Umpire1,
-              Umpire2,
+              data[i][13],
+              data[i][14],
+              umpire1,
+              umpire2,
             ],
           };
           // callback
